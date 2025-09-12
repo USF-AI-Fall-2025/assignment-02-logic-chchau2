8bcd74eb28a88ba29ecb0a51d3aa2b9ccff16b4e
chchau2 <chloeychau@gmail.com>
2025-09-12 15:38:47 -0700

    finished and working part 1

--- DataInvestigator.py
+++ DataInvestigator.py
@@ -1,11 +1,43 @@
+
+import pandas as pd
+
 class DataInvestigator:
 
-    def baseline(col):
-        pass
+    #constructor
+    def __init__(self, df):
+        self.df = df
 
-    def corr(col1, col2):
-        pass
+    #returns most frequent value in the column, none if error
+    def baseline(self, col):
+        try:
+            column = self.df.iloc[:,col]
+            mostFreq = column.mode().iloc[0]
+            return mostFreq
+        except Exception:
+            return None
 
-    def zeroR(col):
-        pass
+    #returns correlation between two columns, none if error
+    def corr(self, col1, col2):
+        try:
+            column1 = self.df.iloc[:,col1]
+            column2 = self.df.iloc[:,col2]
+            return column1.corr(column2)
+        except Exception:
+            return None
 
+    #same as baseline
+    def zeroR(self, col):
+        try:
+            return self.baseline(col)
+        except Exception:
+            return None
+
+
+
+df = pd.read_csv('gallstone.csv')
+di = DataInvestigator(df)
+
+print(di.baseline(1))   #53
+print(di.zeroR(1))      #53
+print(di.corr(0, 1))    #some value
+print(di.baseline(99))  #none, no column 99
